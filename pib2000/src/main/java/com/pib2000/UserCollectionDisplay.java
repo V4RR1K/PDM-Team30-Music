c836c8c984431822511c262de58104e7649fb503
lso2973 <lso2973@rit.edu>
2023-03-21 10:30:56 -0400

    Add files for searching for songs and displaying a user's collections

---
+++ pib2000/src/main/java/com/pib2000/UserCollectionDisplay.java
@@ -0,0 +1,11 @@
+package com.pib2000;
+
+/**
+ * Class intended to perform and process SQL
+ * queries related to displaying the collections
+ * that a user has.
+ *
+ * @author Lily O'Carroll [lso2973@rit.edu]
+ */
+public class UserCollectionDisplay {
+}
