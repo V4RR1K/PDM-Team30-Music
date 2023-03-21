d51706095a07d36e4c5f59e0f9e2bd4118877179
soweeby <rgnunna@gmail.com>
2023-03-21 09:37:27 -0400

    sample sql class added

---
+++ pib2000/src/main/java/com/pib2000/MainInit.java
@@ -0,0 +1,64 @@
+package com.pib2000;
+import com.jcraft.jsch.*;
+
+import java.sql.*;
+import java.util.*;
+
+public class MainInit {
+
+    private static String url = "";
+    public static void main(String[] args) throws SQLException {
+        int lport = 54390;
+        String rhost = "starbug.cs.rit.edu";
+        int rport = 5432;
+        String user = ""; //change to your username
+        String password = ""; //change to your password
+        String databaseName = ""; //change to your database name
+
+        String driverName = "org.postgresql.Driver";
+        Connection conn = null;
+        Session session = null;
+        try {
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            session = jsch.getSession(user, rhost, 22);
+            session.setPassword(password);
+            session.setConfig(config);
+            session.setConfig("PreferredAuthentications","publickey,keyboard-interactive,password");
+            session.connect();
+            System.out.println("Connected");
+            int assigned_port = session.setPortForwardingL(lport, "localhost", rport);
+            System.out.println("Port Forwarded");
+
+            // Assigned port could be different from 5432 but rarely happens
+            String url = "jdbc:postgresql://localhost:"+ assigned_port + "/" + databaseName;
+
+            System.out.println("database Url: " + url);
+            Properties props = new Properties();
+            props.put("user", user);
+            props.put("password", password);
+
+            Class.forName(driverName);
+            conn = DriverManager.getConnection(url, props);
+            System.out.println("Database connection established");
+
+            // Do something with the database....
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Closing Database Connection");
+                conn.close();
+            }
+            if (session != null && session.isConnected()) {
+                System.out.println("Closing SSH Connection");
+                session.disconnect();
+            }
+        }
+    }
+
+
+
+}
