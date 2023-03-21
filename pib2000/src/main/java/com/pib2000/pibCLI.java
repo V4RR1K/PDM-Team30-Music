71afb3b7b5ba8ccaee64f3cfa7fcbf5441b6df32
Greg Lynskey <gclynskey@outlook.com>
2023-03-21 09:54:14 -0400

    Greg: Mocking cli

---
+++ pib2000/src/main/java/com/pib2000/pibCLI.java
@@ -0,0 +1,36 @@
+package com.pib2000;
+
+public class pibCLI {
+    private void collectionMenu(){
+        System.out.println("Welcome to the collection menu");
+    }
+
+    private void SearchMenu(){
+        System.out.println("Welcome to the search menu");
+    }
+
+    private void friendsMenu(){
+        System.out.println("Welcome to the friends menu");
+    }
+
+    private void helpMessage(){
+        System.out.println( "1 - Collections\n" +
+                            "2 - Search\n" +
+                            "3 - Friends\n" +
+                            "h - Help");
+    }
+
+    public static void main(String[] args){
+        // Welcome
+
+        // UserLogin or Create Account
+
+        // Menu (Update access date when logged in)
+        /**
+         *      1 - Collections
+         *      2 - Search
+         *      3 - Friends
+         *      h - Help
+         */
+    }
+}
