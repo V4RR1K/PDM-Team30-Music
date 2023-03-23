b4845126a9ef03969550d98f872edb4ae3cbabba
Greg Lynskey <gclynskey@outlook.com>
2023-03-23 13:11:11 -0400

    Greg: Create user almost working, need to get the next ID

--- pib2000/src/main/java/com/pib2000/User.java
+++ pib2000/src/main/java/com/pib2000/User.java
@@ -2,6 +2,7 @@ package com.pib2000;
 
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -16,8 +17,8 @@ public class User {
     private String firstname;
     private String lastname;
     private String email;
-    private String creationDate;
-    private String lastAccess;
+    private Date creationDate;
+    private Date lastAccess;
 
     private final String[] column_names = {"u_id",
             "username",
@@ -42,15 +43,19 @@ public class User {
 
         createUser(this.username, this.password, this.firstname, this.lastname, this.email);
     }
-    private String generateDate(){
-        Date currTime = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        return format.format(currTime);
+    private Date generateDate(){
+        Date d = new Date();
+        return d;
     }
 
     private int nextId(){
         try (StarbugConnection connection = new StarbugConnection()){
-            return 0;
+            String query = "SELECT MAX(u_id) FROM USER AS MAXID";
+            ResultSet rs = connection.doQuery(query);
+            if (rs.next()){
+                return 1 + rs.getInt("MAXID");
+            }
+            return -1;
         } catch (Exception e){
             e.printStackTrace();
             return -1;
@@ -63,7 +68,30 @@ public class User {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
-        this.creationDate = this.generateDate();
+        this.creationDate = generateDate();
+
+        String query = "INSERT INTO User ("
+                + column_names[0] + ", "
+                + column_names[1] + ", "
+                + column_names[2] + ", "
+                + column_names[3] + ", "
+                + column_names[4] + ", "
+                + column_names[5] + ", "
+                + column_names[6] + ", "
+                + column_names[7] + ") "
+                + "VALUES ("
+                + this.id + ", "
+                + this.username + ", "
+                + this.password + ", "
+                + this.firstname + ", "
+                + this.lastname + ", "
+                + this.email + ", "
+                + this.creationDate + ", "
+                + this.lastAccess + ")";
+
+        System.out.println(query);
+
+
 
         // Manually generate create date, login date
         try (StarbugConnection connection = new StarbugConnection()){
@@ -88,10 +116,15 @@ public class User {
             String query = "SELECT * FROM \"User\" WHERE u_id = " + id;
             ResultSet result = connection.doQuery(query);
             if (result.next()){
-                System.out.println(result.getString(column_names[1]));
-                System.out.println(result.getString(column_names[2]));
+                // Set User variables
+                this.id = result.getInt(column_names[0]);
+                this.username = result.getString(column_names[1]);
+                this.password = result.getString(column_names[2]);
+                this.firstname = result.getString(column_names[3]);
+                this.lastname = result.getString(column_names[4]);
+                this.email = result.getString(column_names[5]);
+                this.creationDate = result.getDate(column_names[6]);
             }
-
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -99,6 +132,8 @@ public class User {
 
     public boolean deleteUser( int id ){
         try (StarbugConnection connection = new StarbugConnection()){
+            String query = "DELETE * FROM \"User\" WHERE u_id = " + id;
+            connection.doQuery(query);
             return true;
         } catch (Exception e){
             e.printStackTrace();
@@ -107,12 +142,31 @@ public class User {
     }
 
     public boolean loginUser(){
-        // Update last login date
+        this.lastAccess = generateDate();
+        updateUser(this.id, this);
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User[" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", creationDate='" + creationDate + '\'' +
+                ", lastAccess='" + lastAccess + '\'' +
+                ']';
     }
 
     // User testing
     public static void main(String[] args){
-        User existing = new User(1);
+//        User existing = new User(1);
+//        System.out.println(existing);
+        User newUser = new User("GregLynskey", "fish",
+                "Greg", "Lynskey", "gcl5615@rit.edu");
+
     }
 }
