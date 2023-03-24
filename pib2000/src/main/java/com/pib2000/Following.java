23462e5c3a27b43afdefbcc25e77db463ac41d3e
Merone Delnesa <mtd6620@rit.edu>
2023-03-23 21:55:28 -0400

    added class that handles Follower queries

---
+++ pib2000/src/main/java/com/pib2000/Following.java
@@ -0,0 +1,58 @@
+package com.pib2000;
+
+import java.sql.ResultSet;
+
+/**
+ * Class intended to handle all Follower related queries
+ *
+ * @author Merone Delnesa [mtd6620@rit.edu]
+ */
+
+public class Following {
+    int u_id;
+    int follows;
+
+    public Following(int user_id, int following_id){
+        this.u_id = user_id;
+        this.follows = following_id;
+    }
+
+    private static int getFollowingID(int user_id){
+        try (StarbugConnection cs = new StarbugConnection()){
+            String query = "select follows from \"Following\" where u_id = " + user_id;
+            ResultSet rs = cs.doQuery(query);
+            if(rs.next()){
+                return rs.getInt("follows");
+            }
+            return -1;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    private static void addUserToFollowing(int user_id, int following_id){
+        try (StarbugConnection cs = new StarbugConnection()){
+            String query = "insert into \"Following\" (SELECT " + user_id + ", " + following_id;
+            int rs = cs.doUpdate(query);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void removeUserFromFollowing(int user_id, int following_id){
+        try (StarbugConnection cs = new StarbugConnection ()){
+            String query = "delete from \"Following\" where u_id = " + user_id + " and follows = " + following_id;
+            int rs = cs.doUpdate(query);
+            if (rs > 0){
+                System.out.println("User successfully deleted from Following list");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ pib2000/src/main/java/com/pib2000/Sort.java
@@ -0,0 +1,10 @@
+package com.pib2000;
+
+/**
+ * Class intended to handle all sorting queries
+ *
+ * @author Merone Delnesa
+ */
+
+public class Sort {
+}
