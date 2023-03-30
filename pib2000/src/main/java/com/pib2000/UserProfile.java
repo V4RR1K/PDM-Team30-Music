cbb248cb19ae70578a8eabddfc4cee8a8ec16292
Greg Lynskey <gclynskey@outlook.com>
2023-03-30 10:28:01 -0400

    Greg: divied work

--- pib2000/src/main/java/com/pib2000/UserProfile.java
+++ pib2000/src/main/java/com/pib2000/UserProfile.java
@@ -1,26 +1,27 @@
 package com.pib2000;
 
 public class UserProfile {
-
-    //
+    // Greg
     public static String numCollections(int userID){
         return null;
     }
-
+    // Greg
     public static int numFollowers(int userID){
         return -1;
     }
-
+    // Greg
     public static int numFollowed(int userID){
         return -1;
     }
-
-    public static String topTenArtistsByPlays(int userID){~
+    // Roshan
+    public static String topTenArtistsByPlays(int userID){
         return null;
     }
+    // Roshan
     public static String topTenArtistsByCollection(int userID){
         return null;
     }
+    // Roshan
     public static String topTenArtistsByPlaysAndCollection(int userID){
         return null;
     }
