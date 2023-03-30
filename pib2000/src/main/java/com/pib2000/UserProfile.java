5490f96aacb639b70144bac33daab744ed5ee7ca
Greg Lynskey <gclynskey@outlook.com>
2023-03-30 10:26:48 -0400

    Greg: Stubbed out profile class

---
+++ pib2000/src/main/java/com/pib2000/UserProfile.java
@@ -0,0 +1,27 @@
+package com.pib2000;
+
+public class UserProfile {
+
+    //
+    public static String numCollections(int userID){
+        return null;
+    }
+
+    public static int numFollowers(int userID){
+        return -1;
+    }
+
+    public static int numFollowed(int userID){
+        return -1;
+    }
+
+    public static String topTenArtistsByPlays(int userID){~
+        return null;
+    }
+    public static String topTenArtistsByCollection(int userID){
+        return null;
+    }
+    public static String topTenArtistsByPlaysAndCollection(int userID){
+        return null;
+    }
+}
