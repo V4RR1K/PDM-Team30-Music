1b3dcda093b3a51edf579ea0b7c4c703c6ffb95b
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2023-04-06 10:00:26 -0400

    Added recommend class

---
+++ pib2000/src/main/java/com/pib2000/Recommendation.java
@@ -0,0 +1,25 @@
+package com.pib2000;
+
+public class Recommendation {
+
+    //Andromeda
+    public static String top50SongsFor30Days(){
+
+        return null;
+    }
+
+    public static String top50SongsOfFriends(int userID){
+
+        return null;
+    }
+
+    //Andromeda
+    public static String top5GenresOfMonth(){
+
+        return null;
+    }
+
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
