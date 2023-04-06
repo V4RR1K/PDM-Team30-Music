1276679e41e2035cbaf0f4aef3a10c297ade0146
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2023-04-05 20:18:32 -0400

    Added in top ten artists by number of plays and collection additions

--- pib2000/src/main/java/com/pib2000/UserProfile.java
+++ pib2000/src/main/java/com/pib2000/UserProfile.java
@@ -1,6 +1,11 @@
 package com.pib2000;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserProfile {
     // Andromeda
@@ -97,13 +102,80 @@ public class UserProfile {
         catch (Exception e) {}
         return sb.toString();
     }
-    // Andromeda
+    /*
+    Andromeda
+    Ideas to improve:
+        - Do it in one SQL query
+    */
     public static String topTenArtistsByPlaysAndCollection(int userID){
-        return null;
+        //top 10 artists by additions to collection
+        String col_query = "select distinct(a.\"Name\"), count(a.ar_id)\n" +
+                "from \"Song_in_collection\" lt\n" +
+                "inner join \"Produces_s\" ps\n" +
+                "on lt.s_id = ps.s_id\n" +
+                "inner join \"Artist\" a\n" +
+                "on ps.ar_id = a.ar_id\n" +
+                "where lt.u_id = " + userID + "\n" +
+                "group by a.\"Name\"\n" +
+                "order by count(a.ar_id) desc\n";
+        String play_query = "select distinct(a.\"Name\"), count(a.ar_id)\n" +
+                "from \"Listened_to\" lt\n" +
+                "inner join \"Produces_s\" ps\n" +
+                "on lt.s_id = ps.s_id\n" +
+                "inner join \"Artist\" a\n" +
+                "on ps.ar_id = a.ar_id\n" +
+                "where lt.u_id = " + userID + "\n" +
+                "group by a.\"Name\"\n" +
+                "order by count(a.ar_id) desc\n";
+        HashMap<String, Integer> artist_num = new HashMap<>();
+        try (StarbugConnection conn = new StarbugConnection()) {
+            ResultSet rs = conn.doQuery(col_query);
+            while (rs.next()) {
+                String name = rs.getString("Name");
+                int amnt = rs.getInt("count");
+                Integer name_amnt = artist_num.get(name);
+                if(artist_num.get(name) == null)
+                    name_amnt = 0;
+                artist_num.put(name, name_amnt + amnt);
+            }
+
+            rs = conn.doQuery(play_query);
+            while (rs.next()) {
+                String name = rs.getString("Name");
+                int amnt = rs.getInt("count");
+                Integer name_amnt = artist_num.get(name);
+                if(artist_num.get(name) == null)
+                    name_amnt = 0;
+                artist_num.put(name, name_amnt + amnt);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(artist_num.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Collections.reverse(list);
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : list){
+            if(count == 10)
+                break;
+            sb.append(entry.getKey());
+            sb.append(" ");
+            sb.append(entry.getValue());
+            sb.append("\n");
+            count++;
+        }
+        return sb.toString();
     }
 
-
     public static void main(String[] args){
-        System.out.println(numCollections(3));
+        System.out.println(topTenArtistsByCollection(2));
+        System.out.println();
+        System.out.println(topTenArtistsByPlays(2));
+        System.out.println();
+        System.out.println(topTenArtistsByPlaysAndCollection(2));
     }
 }
