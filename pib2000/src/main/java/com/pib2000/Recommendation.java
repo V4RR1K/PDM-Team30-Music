9bee5a6eedeb16d23d4e9e851971aaa4b28f0e3d
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2023-04-13 10:41:01 -0400

    Added top 5 gneres

--- pib2000/src/main/java/com/pib2000/Recommendation.java
+++ pib2000/src/main/java/com/pib2000/Recommendation.java
@@ -33,6 +33,7 @@ public class Recommendation {
         return sb.toString();
     }
 
+    //Andromeda
     public static String top50SongsOfFriends(int userID){
 
         return null;
@@ -40,11 +41,36 @@ public class Recommendation {
 
     //Andromeda
     public static String top5GenresOfMonth(){
+        String query = "select distinct(g.\"genrename\"), count(g.g_id)\n" +
+        "from \"Listened_to\" lt\n" +
+        "inner join \"Song\" s on lt.s_id = s.s_id\n" +
+        "inner join \"Genre_s\" gs on gs.s_id = s.s_id\n" +
+        "inner join \"Genre\" g on g.g_id = gs.g_id\n" +
+        "where extract(MONTH from lt.listened_to_datetime) = extract(MONTH from now())\n" +
+        "group by g.\"genrename\"\n" +
+        "order by count(g.g_id) desc\n" +
+        "limit 5";
+        StringBuilder sb = new StringBuilder();
+        try (StarbugConnection conn = new StarbugConnection()) {
+            ResultSet rs = conn.doQuery(query);
 
-        return null;
+            int num = 1;
+            while (rs.next()) {
+                sb.append(num);
+                sb.append(": ");
+                sb.append(rs.getString("genrename"));
+                sb.append(" ");
+                sb.append(rs.getString("count"));
+                sb.append("\n");
+                num++;
+            }
+            return sb.toString();
+        }
+        catch (Exception e) {}
+        return sb.toString();
     }
 
     public static void main(String[] args){
-        System.out.println(top50SongsFor30Days());
+        System.out.println(top5GenresOfMonth());
     }
 }
