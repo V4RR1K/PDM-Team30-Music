d4836c171787daaf19a213b4d043350f064905df
Greg Lynskey <gclynskey@outlook.com>
2023-04-16 19:07:57 -0400

    Greg: added data objects into R

--- Data Analysis/Analytics.R
+++ Data Analysis/Analytics.R
@@ -9,6 +9,7 @@ library(ggplot2)
 most_listened <- read.csv("mostlistened_year.csv", header=TRUE)
 # - Friend correlation to Music Genre
 #   - Hypothesis: Do less followers lead to listening to sad music
-
+friend_genre <- read.csv("friend_genre.csv", header=TRUE)
 # - Highest paid artists (pib2000 pays artists $0.02 per listen)
 #   - Hypothesis: Are higher paid artists rated higher?
+artist_rating <- read.csv("artist_rating.csv", header=TRUE)
