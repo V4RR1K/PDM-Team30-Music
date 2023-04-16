91201b7fb25126932457148249efcd0e980226d6
Greg Lynskey <gclynskey@outlook.com>
2023-04-16 18:16:13 -0400

    Greg: Stubbed out report class

---
+++ pib2000/src/main/java/com/pib2000/Report.java
@@ -0,0 +1,20 @@
+package com.pib2000;
+
+
+
+public class Report {
+
+    public static void listenedToGenre(){
+        return;
+    }
+
+    public static void frientToGenre(){
+        return;
+    }
+    public static void popularArtistRating(){
+        return;
+    }
+    public static void main(String[] args){
+        System.out.println("hello slut");
+    }
+}
