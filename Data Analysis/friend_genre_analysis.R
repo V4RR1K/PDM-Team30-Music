cedbb3d63fe3fcdd66500edd65794bdfb7f9c1be
Greg Lynskey <gclynskey@outlook.com>
2023-04-18 18:10:38 -0400

    Greg: Got users top genres

--- Data Analysis/friend_genre_analysis.R
+++ Data Analysis/friend_genre_analysis.R
@@ -8,6 +8,12 @@ library(ggplot2)
 #   - DataNeeded: Count of follow occurances (get min) vs genre
 friend_genre <- read.csv("friend_genre.csv", header=TRUE)
 
+fri_gen_df <- data.frame(
+  friend_genre$follows,
+  friend_genre$genrename
+)
+colnames(fri_gen_df) <- c("user", "genre")
+
 follow_counter <- table(friend_genre$follows)
 fc_df <- as.data.frame(follow_counter)
 
@@ -15,3 +21,14 @@ colnames(fc_df) <- c("User", "NumFriends")
 fc_df_min <- fc_df[order(fc_df$NumFriends),]
 fc_df_max <- fc_df[order(-fc_df$NumFriends),]
 
+# Get users most listened to genres
+user_top_genre <- function(u_id){
+  top <- fri_gen_df[fri_gen_df$user == u_id,]
+  top_counter_t <- table(top$genre)
+  top_coutner_df <- as.data.frame(top_counter_t)
+  colnames(top_coutner_df) <- c("genere", "plays")
+  top_coutner_df <- top_coutner_df[order(-top_coutner_df$plays),]
+  return(top_coutner_df[1:5,])
+}
+
+test <- user_top_genre(2)
